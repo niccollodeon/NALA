f600eec1ad18ca666d125fe10415254732b7126b
niccollodeon <niccollodayrit25@gmail.com>
2025-02-12 12:50:34 +0800

    new trans

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+import languages from './language.js'
 // navigation  menu js
 function openNav() {
     $("#myNav").addClass("menu_width");
@@ -17,16 +18,6 @@ function closeNav() {
     easing: 'ease-in-out', // Easing function
     reset: true // Whether the animation should reset when scrolling back up
   });
-
-// get current year
-
-function displayYear() {
-    var d = new Date();
-    var currentYear = d.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-displayYear();
-
 
 
 //client section owl carousel
@@ -75,23 +66,159 @@ $('.slider_btn_next').on('click', function (e) {
 /** google_map js **/
 
 function myMap() {
-    var mapProp = {
+    const mapProp = {
         center: new google.maps.LatLng(40.712775, -74.005973),
         zoom: 18,
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    const map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
-function translateText() {
-    const text = document.getElementById('textToTranslate').value;
-    fetch('/translate', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({sentence: text})
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('translation').textContent = data.translation;
-    })
-    .catch(error => console.error('Error:', error));
+//dropdown nung translator box
+const dropdowns = document.querySelectorAll(".dropdown-container"),
+  inputLanguageDropdown = document.querySelector("#input-language"),
+  outputLanguageDropdown = document.querySelector("#output-language");
+
+
+function populateDropdown(dropdown, options) {
+  dropdown.querySelector("ul").innerHTML = "";
+  options.forEach((option) => {
+    const li = document.createElement("li");
+    const title = option.name + " (" + option.native + ")";
+    console.log(title)
+    li.innerHTML = title;
+    li.dataset.value = option.code;
+    li.classList.add("option");
+    dropdown.querySelector("ul").appendChild(li);
+  });
 }
+
+populateDropdown(inputLanguageDropdown, languages);
+populateDropdown(outputLanguageDropdown, languages);
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    dropdown.classList.add("active");
+  });
+
+  dropdown.querySelectorAll(".option").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      //remove active class from current dropdowns
+      dropdown.querySelectorAll(".option").forEach((item) => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+      const selected = dropdown.querySelector(".selected");
+      selected.innerHTML = item.innerHTML;
+      selected.dataset.value = item.dataset.value;
+      translate();
+    });
+  });
+});
+document.addEventListener("click", (e) => {
+  dropdowns.forEach((dropdown) => {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove("active");
+    }
+  });
+});
+
+const swapBtn = document.querySelector(".swap-position"),
+  inputLanguage = inputLanguageDropdown.querySelector(".selected"),
+  outputLanguage = outputLanguageDropdown.querySelector(".selected"),
+  inputTextElem = document.querySelector("#input-text"),
+  outputTextElem = document.querySelector("#output-text");
+
+swapBtn.addEventListener("click", (e) => {
+  const temp = inputLanguage.innerHTML;
+  inputLanguage.innerHTML = outputLanguage.innerHTML;
+  outputLanguage.innerHTML = temp;
+
+  const tempValue = inputLanguage.dataset.value;
+  inputLanguage.dataset.value = outputLanguage.dataset.value;
+  outputLanguage.dataset.value = tempValue;
+
+  //swap text
+  const tempInputText = inputTextElem.value;
+  inputTextElem.value = outputTextElem.value;
+  outputTextElem.value = tempInputText;
+
+  translate();
+});
+
+function translate() {
+  const inputText = inputTextElem.value;
+  const inputLanguage =
+    inputLanguageDropdown.querySelector(".selected").dataset.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(
+    inputText,
+  )}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      outputTextElem.value = json[0].map((item) => item[0]).join("");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+inputTextElem.addEventListener("input", (e) => {
+  const inputChars = document.getElementById('input-chars')
+
+  inputChars.textContent = inputTextElem.value.length
+  //limit input to 5000 characters
+  if (inputTextElem.value.length > 5000) {
+    inputTextElem.value = inputTextElem.value.slice(0, 5000);
+  }
+  translate();
+});
+
+const uploadDocument = document.querySelector("#upload-document"),
+  uploadTitle = document.querySelector("#upload-title");
+
+uploadDocument.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (
+    file.type === "application/pdf" ||
+    file.type === "text/plain" ||
+    file.type === "application/msword" ||
+    file.type ===
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  ) {
+    uploadTitle.innerHTML = file.name;
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      inputTextElem.value = e.target.result;
+      translate();
+    };
+  } else {
+    alert("Please upload a valid file");
+  }
+});
+
+const downloadBtn = document.querySelector("#download-btn");
+
+downloadBtn.addEventListener("click", (e) => {
+  const outputText = outputTextElem.value;
+  const outputLanguage =
+    outputLanguageDropdown.querySelector(".selected").dataset.value;
+  if (outputText) {
+    const blob = new Blob([outputText], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.download = `translated-to-${outputLanguage}.txt`;
+    a.href = url;
+    a.click();
+  }
+});
+
+const inputChars = document.querySelector("#input-chars");
+
+inputTextElem.addEventListener("input", (e) => {
+  inputChars.innerHTML = inputTextElem.value.length;
+});
+
+
---
+++ js/language.js
@@ -0,0 +1,45 @@
+const languages = [
+    {
+      no: "0",
+      name: "Auto",
+      native: "Detect",
+      code: "auto",
+    },
+    {
+      no: "1",
+      name: "Tagalog",
+      code: "tg",
+    },
+    {
+      no: "2",
+      name: "Cebuano",
+      native: "Binisaya-Bisaya",
+      code: "Ceb",
+    },
+    {
+      no: "3",
+      name: "Ilocano",
+      native: "Ilokano",
+      code: "il",
+    },
+    {
+      no: "4",
+      name: "Pangasinan",
+      code: "pg",
+    },
+    {
+      no: "5",
+      name: "Hilagaynon",
+      native: "Ilonggo",
+      code: "hg",
+    },
+    {
+      no: "6",
+      name: "Waray",
+      native: "Waray-waray",
+      code: "wr",
+    },
+  ];
+  
+
+  export default languages
